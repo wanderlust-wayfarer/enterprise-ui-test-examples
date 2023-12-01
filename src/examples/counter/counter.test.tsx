2b56c74ed1372b249503972fa6ced1d8b870f504
Michael Pay <123031523+wanderlust-wayfarer@users.noreply.github.com>
2023-11-30 23:58:22 -0800

    vitest counter test & setup + utilities "global"ish imports

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,25 @@
-import { screen, render } from '@testing-library/react';
+// @vitest-environment happy-dom
+
+import { screen, fireEvent } from '@testing-library/react';
+import { render } from './test/utilities';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount.textContent).toBe('0');
+});
 
-test.todo(
-  'it should increment when the "Increment" button is pressed',
-  async () => {},
-);
+test('it should increment when the "Increment" button is pressed', async () => {
+  const { user } = render(<Counter />);
+
+  const currentCount = screen.getByTestId('current-count');
+  const incrementButton = screen.getByRole('button', { name: 'Increment' });
+  // Alternatively: fireEvent.click(incrementButton);
+  // However, user better mirrors a sequence of actions a user would do
+  // (such as key down, then key up) to perform the action.
+  await user.click(incrementButton);
+  // Using testing library extensions imported in ./test/setup.ts
+  expect(currentCount).toHaveTextContent('1');
+});
--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -1,5 +1,14 @@
-export {};
+import { render as renderComponent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import type { ReactElement } from 'react';
 
-/**
- * For a complete example, see: test/utilities.ts
- */
+type RenderOptions = Parameters<typeof renderComponent>[1];
+
+export * from '@testing-library/react';
+
+export const render = (ui: ReactElement, options?: RenderOptions) => {
+  return {
+    ...renderComponent(ui, options),
+    user: userEvent.setup(),
+  };
+};
