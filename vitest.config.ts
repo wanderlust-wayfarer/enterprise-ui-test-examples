b7353f4642496b124a5449188552f1d7671850f3
Michael Pay <123031523+wanderlust-wayfarer@users.noreply.github.com>
2023-12-31 22:11:30 -0800

    added coverage testing and reporting to ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       ['**/*.component.test.ts', 'jsdom'],
     ],
     coverage: {
-      statements: 54.92,
+      statements: 54.91,
       thresholdAutoUpdate: true,
       include: ['src/**/*'],
       exclude: [
